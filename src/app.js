85cdd85c84a110583027491d5bb84a823269e4fe
stanko.jevtic <stanko.jevtic@htecgroup.com>
2020-07-06 16:31:08 +0200

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+//prod env - heroku port
+const port = process.env.PORT || 3000
+
 //define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -101,6 +104,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
